720c08673129a9fcd03c553cc15e22fa313847d0
Abu Saeid <abusaeid95@gmail.com>
2021-11-18 11:24:15 +0600

    final

---
+++ app.js
@@ -0,0 +1,23 @@
+const navslide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav_link");
+  const navLinks = document.querySelectorAll(".nav_link li");
+  // Toggle Nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //   Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0
+        }s`;
+      }
+    });
+    burger.classList.toggle('toggle')
+  });
+};
+
+navslide();
